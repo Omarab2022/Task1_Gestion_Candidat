d2335b8598c31ea3424e9fde88d287613990e68f
OMAR ABARRA <abarraomar@gmail.com>
2024-07-03 10:36:53 +0100

    create java entities

---
+++ Task1_BACKEND/src/main/java/Task1_Backend/Task1/Entity/CertificateTemplate.java
@@ -0,0 +1,12 @@
+package Task1_Backend.Task1.Entity;
+
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+public class CertificateTemplate {
+}
